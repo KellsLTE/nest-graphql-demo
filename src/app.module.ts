1e68fab056fd7ecbd19ba7bbc26d349781212668
Favour Max-Oti <maxotif@gmail.com>
2022-06-27 07:34:44 +0100

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PetsModule } from './pets/pets.module';
+
+@Module({
+  imports: [
+    PetsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
